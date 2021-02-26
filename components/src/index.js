b516ee54cdea5fb81e70b8addea3beaea63f728c
Ashish Sheoran <sheoranashu079@gmail.com>
2021-02-26 13:26:12 -0500

    Added ApprovalCard component

--- components/src/index.js
+++ components/src/index.js
@@ -2,30 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail
-                author="Sam"
-                timeAgo="Today at 4:35 PM"
-                image={faker.image.image()}
-                comment="Awesome post!"
-            />
+            <ApprovalCard>
+                <h4>Warning!</h4>
+                Are you sure you want to do this?
+            </ApprovalCard>
 
-            <CommentDetail
-                author="Axel"
-                timeAgo="Today at 2:00 AM"
-                image={faker.image.image()}
-                comment="What a splendid post!"
-            />
+            <ApprovalCard>
+                <CommentDetail
+                    author="Sam"
+                    timeAgo="Today at 4:35 PM"
+                    image={faker.image.image()}
+                    comment="Awesome post!"
+                />
+            </ApprovalCard>
 
-            <CommentDetail
-                author="Jane"
-                timeAgo="Yesterday at 11:00 PM"
-                image={faker.image.image()}
-                comment="Thank you!"
-            />
+            <ApprovalCard>
+                <CommentDetail
+                    author="Axel"
+                    timeAgo="Today at 2:00 AM"
+                    image={faker.image.image()}
+                    comment="What a splendid post!"
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="Jane"
+                    timeAgo="Yesterday at 11:00 PM"
+                    image={faker.image.image()}
+                    comment="Thank you!"
+                />
+            </ApprovalCard>
         </div>
     )
 }
