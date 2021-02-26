09daf99eba29a7422e992b61d3a6ccc1416636fd
Ashish Sheoran <sheoranashu079@gmail.com>
2021-02-26 10:41:04 -0500

    Created first JSX elements

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,10 +1,20 @@
 // Import the React and ReactDOM libraries
 import React from 'react';
-import ReactDOM from 'react';
+import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
-    return ();
+    const buttonText = { text: 'Click Me' };
+    const labelText = 'Enter Name: ';
+    return (
+        <div>
+            <label className="label" forName="name"> {labelText} </label>
+
+            <input id="name" type="text" />
+
+            <button style={{ backgroundColor: 'lightblue', color: 'white', border: 'none', cursor: 'pointer' }}> {buttonText.text} </button>
+        </div>
+    );
 }
 
 // Take the react component and show it on screen
