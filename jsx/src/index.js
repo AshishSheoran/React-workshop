47c5443e80d0e8a55413cffd66e87a20024b36bc
Ashish Sheoran <sheoranashu079@gmail.com>
2021-02-26 10:29:36 -0500

    Creating first React JSX component

---
+++ jsx/src/index.js
@@ -0,0 +1,14 @@
+// Import the React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react';
+
+// Create a react component
+const App = () => {
+    return ();
+}
+
+// Take the react component and show it on screen
+ReactDOM.render(
+    <App />,                                    // Take this react component
+    document.querySelector('#root')             // and show this component on HTML page's (#root) id.
+);
