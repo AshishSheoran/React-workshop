fc2ff3d8b4b6db70d44ce573cd6f18c35ee17f59
Ashish Sheoran <sheoranashu079@gmail.com>
2021-02-28 16:04:46 -0500

    Updated

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,13 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    // THIS is the ONLY TIME we do direct assignment to "this.state"
+    this.state = {
+      lat: null
+    };
+
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
+      (position) => {
+        // We called "setState"
+        this.setState({ lat: position.coords.latitude})
+
+
+      },
       (err) => console.log(err)
     );
+  }
 
-    return <div>Latitude: </div>
+  
+
+  // React says we have to define a render method
+  render() {
+    return <div>Latitude: {this.state.lat} </div>
   }
 };
 
