da29e8dfd5b10864330e35261a5c8e744c57963f
Ashish Sheoran <sheoranashu079@gmail.com>
2021-03-01 12:26:27 -0500

    Added loading spinner

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,38 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  // Equivalent to defining a construtor and defining state inside constructor.
+  state = { lat: null, errorMessage: '' };
 
-    // THIS is the ONLY TIME we do direct assignment to "this.state"
-    this.state = {
-      lat: null,
-      errorMessage: ''
-    };
-
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        // To update lat object, We called "setState"
-        this.setState({ lat: position.coords.latitude })
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message })
-      }
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
   }
 
   // React says we have to define a render method
   render() {
-      if (this.state.errorMessage && !this.state.lat) {
-        return <div>Error: {this.state.errorMessage} </div>
-      } 
-      else if (!this.state.errorMessage && this.state.lat) {
-        return <div>Latitude: {this.state.lat}</div>
-      }
-      else {
-        return <div>Loading!</div>
-      }
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage} </div>
+    }
+    else if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />
+    }
+    else {
+      return <Spinner  />
+    }
   }
 };
 
