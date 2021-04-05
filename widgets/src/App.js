af891ef1e1e488dad3c7b2e21020f1ff37359281
Ashish Sheoran <sheoranashu079@gmail.com>
2021-04-04 21:52:50 -0400

    Added Accordian component

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,5 +1,27 @@
 import React from 'react';
+import Accordian from './components/Accordian';
 
-const App = () => {};
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a front end JavaScript framework.',
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is a favourite JS library among engineers.',
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use React by creating component.',
+    },
+];
+
+const App = () => {
+    return (
+        <div>
+            <Accordian items={items} />
+        </div>
+    );
+};
 
 export default App;
--- widgets/src/index.js
+++ widgets/src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, querySelector('root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
