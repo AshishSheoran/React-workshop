641df1b11f345e1d4deaca9b21636656da3e7419
Ashish Sheoran <sheoranashu079@gmail.com>
2021-04-06 17:01:36 -0400

    Added Dropdown widget

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,27 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import Accordian from './components/Accordian';
-import Search from './components/Search';
+//import Search from "./components/Search";
+import Dropdown from './components/Dropdown';
 
-const items = [
+// const items = [
+//     {
+//         title: 'What is React?',
+//         content: 'React is a front end JavaScript framework.',
+//     },
+//     {
+//         title: 'Why use React?',
+//         content: 'React is a favourite JS library among engineers.',
+//     },
+//     {
+//         title: 'How do you use React?',
+//         content: 'You use React by creating component.',
+//     },
+// ];
+
+const options = [
     {
-        title: 'What is React?',
-        content: 'React is a front end JavaScript framework.',
+        label: 'The Color Red',
+        value: 'red',
     },
     {
-        title: 'Why use React?',
-        content: 'React is a favourite JS library among engineers.',
+        label: 'The Color Green',
+        value: 'green',
     },
     {
-        title: 'How do you use React?',
-        content: 'You use React by creating component.',
+        label: 'A Shade of Blue',
+        value: 'blue',
     },
 ];
 
 const App = () => {
+    const [selected, setSelected] = useState(options[0]);
+    const [showDropdown, setShowDropdown] = useState(true);
+
     return (
         <div>
             {/* <Accordian items={items} /> */}
-            <Search />
+            {/* <Search /> */}
+            <button onClick={() => setShowDropdown(!showDropdown)}>
+                Toggle Dropdown
+            </button>
+            {showDropdown ? (
+                <Dropdown
+                    selected={selected}
+                    onSelectedChange={setSelected}
+                    options={options}
+                />
+            ) : null}
         </div>
     );
 };
