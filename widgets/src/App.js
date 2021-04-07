168dde83790f8416fde26daaba89eb5dde5821cb
Ashish Sheoran <sheoranashu079@gmail.com>
2021-04-07 14:22:30 -0400

    Added Routes for widgets

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -3,6 +3,22 @@ import Accordian from './components/Accordian';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
+import Route from './components/Route';
+
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a front end javaScript framework.',
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is a favourite JS library among engineers.',
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use React by creating components.',
+    },
+];
 
 const options = [
     {
@@ -20,9 +36,26 @@ const options = [
 ];
 
 const App = () => {
+    const [selected, setSelected] = useState(options[0]);
     return (
         <div>
-            <Translate />
+            <Route path="/">
+                <Accordian items={items} />
+            </Route>
+            <Route path="/list">
+                <Search />
+            </Route>
+            <Route path="/dropdown">
+                <Dropdown
+                    label="Select a color"
+                    options={options}
+                    selected={selected}
+                    onSelectedChange={setSelected}
+                />
+            </Route>
+            <Route path="/translate">
+                <Translate />
+            </Route>
         </div>
     );
 };
