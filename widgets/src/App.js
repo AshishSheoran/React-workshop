8a33a78175a88be2f7afdc41ab1de4ed8ac15232
Ashish Sheoran <sheoranashu079@gmail.com>
2021-04-11 22:56:53 -0400

    Something

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -6,60 +6,62 @@ import Translate from './components/Translate';
 import Route from './components/Route';
 import Header from './components/Header';
 
+// Hello
+
 const items = [
-    {
-        title: 'What is React?',
-        content: 'React is a front end javaScript framework.',
-    },
-    {
-        title: 'Why use React?',
-        content: 'React is a favourite JS library among engineers.',
-    },
-    {
-        title: 'How do you use React?',
-        content: 'You use React by creating components.',
-    },
+	{
+		title: 'What is React?',
+		content: 'React is a front end javaScript framework.',
+	},
+	{
+		title: 'Why use React?',
+		content: 'React is a favourite JS library among engineers.',
+	},
+	{
+		title: 'How do you use React?',
+		content: 'You use React by creating components.',
+	},
 ];
 
 const options = [
-    {
-        label: 'The Color Red',
-        value: 'red',
-    },
-    {
-        label: 'The Color Green',
-        value: 'green',
-    },
-    {
-        label: 'A Shade of Blue',
-        value: 'blue',
-    },
+	{
+		label: 'The Color Red',
+		value: 'red',
+	},
+	{
+		label: 'The Color Green',
+		value: 'green',
+	},
+	{
+		label: 'A Shade of Blue',
+		value: 'blue',
+	},
 ];
 
 const App = () => {
-    const [selected, setSelected] = useState(options[0]);
-    return (
-        <div>
-            <Header />
-            <Route path="/">
-                <Accordian items={items} />
-            </Route>
-            <Route path="/list">
-                <Search />
-            </Route>
-            <Route path="/dropdown">
-                <Dropdown
-                    label="Select a color"
-                    options={options}
-                    selected={selected}
-                    onSelectedChange={setSelected}
-                />
-            </Route>
-            <Route path="/translate">
-                <Translate />
-            </Route>
-        </div>
-    );
+	const [selected, setSelected] = useState(options[0]);
+	return (
+		<div>
+			<Header />
+			<Route path='/'>
+				<Accordian items={items} />
+			</Route>
+			<Route path='/list'>
+				<Search />
+			</Route>
+			<Route path='/dropdown'>
+				<Dropdown
+					label='Select a color'
+					options={options}
+					selected={selected}
+					onSelectedChange={setSelected}
+				/>
+			</Route>
+			<Route path='/translate'>
+				<Translate />
+			</Route>
+		</div>
+	);
 };
 
 export default App;
